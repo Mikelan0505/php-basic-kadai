4991822b9e3a3952fdb5c78f36ee1428f871f264
みけらん <exiga505@hotmail.co.jp>
2025-07-10 19:18:42 +0900

    Add files via upload

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -9,29 +9,31 @@
     // Step4: ソート対象の配列
     $nums = [15, 4, 18, 23, 10];
 
-    //Step2: 配列のソートに使える標準関数
-    //昇順⇒sort、降順⇒rsortを使用。
+    // Step2: PHPマニュアルで調べた sort / rsort を使う
 
-    // Step3: ソート関数を定義
-    function sort_2way($array, $order) {
-      if ($order) {
-        echo '昇順にソートします。<br>';
-        sort($array); // 昇順
+    // Step3: ソート関数（第2引数で昇順/降順を切り替え）
+    function sort_2way(array $array, bool $order): void {
+      if ($order === true) {
+        echo "昇順にソートします。<br>";
+        sort($array); 
       } else {
-        echo '降順にソートします。<br>';
-        rsort($array); // 降順
+        echo "降順にソートします。<br>";
+        rsort($array); 
       }
 
-      // 結果を1行ずつ表示
+      // 結果を表示
       foreach ($array as $value) {
-        echo $value . '<br>';
+        echo $value . "<br>";
       }
     }
 
-    // Step5: 関数を呼び出し（TRUEなら昇順）
-    sort_2way($nums, true);
-    echo '<hr>';
-    sort_2way($nums, false);
+    // Step5: 関数を呼び出す
+    sort_2way($nums, true);  // 昇順
+    echo "<hr>";
+    sort_2way($nums, false); // 降順
   ?>
 </body>
 </html>
+
+
+
